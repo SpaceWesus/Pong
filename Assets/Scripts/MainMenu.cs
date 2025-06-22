46bafc684c17d28c4c2cccd5799c034add73370d
Jayden Robles <98366817+SpaceWesus@users.noreply.github.com>
2025-06-21 19:10:57 -0700

    Pong Design Doc + Main & Level Selection Menus
    # NOTES
    - When creating your levels/scenes. Make sure you name the actual scene you are working on, "Level XXX", that way the level selection screen can directly open up that scene.
    
    # Additions
    - Created a main menu scene.
    - Added a "Play" button that starts "level 1".
    - Added a "Quit" button that quits when the project is built.
    - Added a "Level Selection" button that swaps the "Main Menu" around for the "Level Select Menu".
    - Added functionality to the "Level 1 - Pong" button that, when selected, starts level 1 via a string reference within the OnClick() Method in the inspector.
    
    # Changes
    
    # TODO
    - Create "Level 1" AKA "Pong"
    - Create a controllable Paddle with a 2D RigidBody and 2D Hitbox
    - Create a ball that is not affected by gravity nor friction and is spawned in the beginning of a match and after each point.
    - When the ball hits a paddle it flies off in a semi-random direction with a random velocity between an upper and lower bound of speed.
    - The game finishes after someone scores 5 times.  If you lose, you repeat the level.
    - Score is kept in the top portion of each players side of the screen.
    - The screen is divided vertically in half by a dashed line.
    - Score colliders are just behind the paddles that destroy the ball when it comes into contact with it and increments the opposite player's score by 1.
    - A 5 second delay is added such that after the ball is scored, you have 5 seconds to recollect yourself before the next Point is played for.
    - Basic Enemy Paddle AI that tries to deflect the ball but has some variance in its ability to actually block it. Meaning sometimes it undershoots the  ball and sometimes it overshoots the ball.
    
    # Scope Creep Ideas
    - Win by 2? The player/AI needs to win by 2 in order to complete the level.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,23 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame()
     {
-        
+        // Can start with "Level 1 - BaseGame" Scene via a string reference
+        SceneManager.LoadScene("Level 1");
+
+        // Alternative LoadScene "Methodology"
+        // Will be useful when in game already and you want to move on to the next after winning the current level.
+        // Can also load the next scene in the order via the build settings in Unity (Next scene would be "Level 1")
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
